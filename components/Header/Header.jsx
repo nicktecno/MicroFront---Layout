a9eb883c0ba86e41b8ad12b8b57a766d6888b962
NicolasCouto <nicolas.couto@cazco.digital>
2023-01-16 10:19:23 -0300

    update: a to Link

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -163,13 +163,13 @@ const HeaderComponent = ({
       <S.box>
         <S.imagens>
           <div className="logoSandwich">
-            <a href="/">
+            <Link href="/">
               <img
                 alt="Cielo"
                 src={Object.values(defaultLayout)[0][0]["logo-img"]}
                 id="logoDefault"
               />
-            </a>
+            </Link>
 
             {/* desktop Menu */}
           </div>
